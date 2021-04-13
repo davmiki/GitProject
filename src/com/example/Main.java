c14f96884d1562e5185943750d14e9cf68eb519c
david <davstern10@gmail.com>
2021-04-13 21:05:14 +0300

    Initial commit

---
+++ src/com/example/Main.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class dcd
+        Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello from GIT!");
+    }
+}
